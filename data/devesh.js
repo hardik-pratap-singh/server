8462d37f33aab866d7c742ca0ef5ca4117cef1fe
hardik-pratap-singh <21bcs090@iiitdmj.ac.in>
2025-04-22 00:36:23 +0530

    get request, post request done, dynamic routing DONE

---
+++ data/devesh.js
@@ -0,0 +1,1 @@
+const vedant = require("../vedant/data1.json") ; 
--- index.js
+++ index.js
@@ -1,31 +1,94 @@
-//Common JS Module // Ecma JS 
+//Common JS Module (const/ let/ var) - nodejs // Ecma JS (import/export) - reactjs
 const express = require("express");
 const app = express();
-const data = require("./data.json") ;
+const user = require("./data.json") ;
+// const db = asgasla dsfjk asd 
+// const users = require("./users.json")
+// "../../"
+// "./"
+// ".././"
 
-const http = require("http")
+// const http = require("http") - node 
+
+app.use(express.json()) ; 
 
 
-app.get("/",(req, res) => {
+
+app.get("/user",(req, res) => {
     // console.log(data) ; 
-    res.json(data);
+    res.json(user);
 })
 
-app.get("/1" ,)
 
-app.post("/about" , (req , res) => {
+
+// /user/id/45
+
+// /user/hardik
+app.get('/user/id' , (req , res) => {
+    res.send("User with Id") ; 
+})
+
+app.get("/user" , (req , res) => {
+    res.send("Get request fulfilled !!")
+})
+
+app.get("/user/:id" ,(req , res) => {
+    const params = req.params ; 
+    const userId = params.id ; 
+
+    //json - user - []
+    // id = userId 
+
+    const user1 = user.find(u => (u.id == userId)) ; 
+
+    // res.send(params.id) ; 
+    res.send(user1); 
+})
+
+// POST , PUT , PATCH , DELETE 
+
+// userName , emailId , password 
+app.post("/user" , (req , res) => {
     const data = req.body ; 
-    //database scripts 
+    console.log(data);  
 
 
-    //db end
-
-    res.json({success : true}) ; 
+    res.send("POST request !!") ; 
 })
+
+
+
+app.get("/user/id/45" , (req , res) => {
+    res.send("user with id 45") ; 
+})
+
+
+// app.get("user/:id", (req , res) => {
+//     res.json("User Not Found") ; 
+
+// })
+
+// /users 
+app.get("/users" , (req , res) => {
+    res.json(users) ; 
+})
+
+
+// app.get("/1" ,)
+
+// app.post("/about" , (req , res) => {
+//     const data = req.body ; 
+//     //database scripts 
+
+
+//     //db end
+
+//     res.json({success : true}) ; 
+// })
 
 
 app.listen(7000  , ()=> {
-    console.log("Server is running @ 8080")
+    console.log("Server is running @ 7000")
 })
 
 
