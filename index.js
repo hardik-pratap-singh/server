0ffa0e459a66afe70835244c8389a4ec8af8d906
hardik-pratap-singh <21bcs090@iiitdmj.ac.in>
2025-04-23 01:06:21 +0530

    patch, delete request done

--- index.js
+++ index.js
@@ -1,65 +1,133 @@
 //Common JS Module (const/ let/ var) - nodejs // Ecma JS (import/export) - reactjs
 const express = require("express");
 const app = express();
-const user = require("./data.json") ;
+const user = require("./data.json");
 // const db = asgasla dsfjk asd 
 // const users = require("./users.json")
 // "../../"
 // "./"
 // ".././"
 
+// const mongoose = require('mongoose') ; 
+
+// PUT 
+// {
+//   "id": 101,
+//   "name": "Clementina Du"
+  
+// },
+
+// PATCH
+// {
+//   "id": 101,
+//   "name": "Clementina Du", 
+//   "username": "Mon",
+//   "email": "Rey.Padberg@karina.biz"
+// },
+
+
 // const http = require("http") - node 
 
-app.use(express.json()) ; 
+app.use(express.json());
+
+//bodyparser
 
 
-
-app.get("/user",(req, res) => {
-    // console.log(data) ; 
-    res.json(user);
-})
-
+// app.
+//     get
+//     ("/user",
+//         (req, res) => {
+//             // console.log(data) ; 
+//             res.json(user);
+//         })
 
 
 // /user/id/45
 
 // /user/hardik
-app.get('/user/id' , (req , res) => {
-    res.send("User with Id") ; 
+app.get('/user/id', (req, res) => {
+    res.send("User with Id");
 })
 
-app.get("/user" , (req , res) => {
-    res.send("Get request fulfilled !!")
+app.get("/user", (req, res) => {
+
+    res.send(user) ; 
+    // res.send("Get request fulfilled !!")
 })
 
-app.get("/user/:id" ,(req , res) => {
-    const params = req.params ; 
-    const userId = params.id ; 
+app.get("/user/300", (req, res) => {
+    res.send("user 300");
+})
 
+app.get("/user/400", (req, res) => {
+    res.send("user 400");
+})
+
+app.get("/user/500", (req, res) => {
+    res.send("user 500");
+})
+
+
+//dyanmic routing
+
+// app.get("/users/:userId/:locationId")
+
+app.get('/rohan/:class/:rollno' , (req , res) => {
+    const class1 = req.params.class ;
+    const rollno = req.params.rollno ; 
+
+    res.send({class1 , rollno}) ; 
+})
+
+app.get("/user/:userId", (req, res) => {
+
+    // req = {
+    //     params: {
+    //         userId: {
+
+    //         }
+    //     }
+    // }
+    const userId = req.params.userId;
+    // const userId = params.id ; 
+    // console.log(userId) ; 
     //json - user - []
     // id = userId 
 
-    const user1 = user.find(u => (u.id == userId)) ; 
+    const user1 = user.find(u => (u.id == userId));
 
     // res.send(params.id) ; 
-    res.send(user1); 
+    res.send(user1);
 })
 
-// POST , PUT , PATCH , DELETE 
+// GET , POST , PUT , PATCH , DELETE 
 
 // userName , emailId , password 
-app.post("/user" , (req , res) => {
-    const data = req.body ; 
-    console.log(data);  
+app.post("/user", (req, res) => {
 
+    const data = req.body;
+    console.log(req);
 
-    res.send("POST request !!") ; 
+    // req : {
+    //     body : {
+    //         "userId" : 
+    //         "password" : 
+    //     }
+    //     params : {
+    //         "userId" : "" , 
+    //         "locationId" : ""
+    //     }
+    // }
+    res.send("POST request !!");
 })
 
 
+// req = {
 
-app.get("/user/id/45" , (req , res) => {
-    res.send("user with id 45") ; 
+// }
+
+app.get("/user/id/45", (req, res) => {
+    res.send("user with id 45");
 })
 
 
@@ -69,8 +137,8 @@ app.get("/user/id/45" , (req , res) => {
 // })
 
 // /users 
-app.get("/users" , (req , res) => {
-    res.json(users) ; 
+app.get("/users", (req, res) => {
+    res.json(users);
 })
 
 
@@ -87,7 +155,49 @@ app.get("/users" , (req , res) => {
 // })
 
 
-app.listen(7000  , ()=> {
+//PATCH request (update document)
+
+// URL : http://localhost:7000/user/101, PATCH
+app.patch("/user/:id" , (req , res) => {
+    const id = req.params.id ; 
+
+    const updates = req.body ; //updated data {name : "Rohan"}
+
+    const doc = user.find(u => u.id = id) ;
+    // console.log("doc")
+    // console.log(doc) ; 
+
+    // db.save() ; 
+    Object.assign(doc , updates) ; 
+    //function // map, filter, find // javascript 
+
+    // console.log(doc) ; 
+    res.send(doc) 
+
+})
+
+
+//DELETE request  - deletes some resource from the db/json
+app.delete("/user/:id" , (req , res) => {
+    //splice method in javascript 
+    const idToDelete = parseInt(req.params.id) ; 
+    // const docToDelete = user.find(u => u.id = idToDelete) ;
+    console.log("idToDelete " , idToDelete) ; 
+
+    const idxToDelete = user.findIndex(u => u.id == idToDelete); 
+
+    console.log("idxToDelete" , idxToDelete) ; 
+    const updatedData = user.splice(idxToDelete , 2) ; 
+
+    // const updatedUser = user.splice()
+
+
+    res.send(updatedData); 
+})
+
+
+
+app.listen(7000, () => {
     console.log("Server is running @ 7000")
 })
 
